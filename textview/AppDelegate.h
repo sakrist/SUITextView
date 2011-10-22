0b03c4fc96d58ec8d54395a74835148782c9084c
Vladimir <sakristx@gmail.com>
2011-10-22 22:51:16 +0300

    Initial Commit

---
+++ textview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  textview
+//
+//  Created by Volodymyr Boichentsov on 10/22/11.
+//  Copyright (c) 2011 www.injoit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ textview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  textview
+//
+//  Created by Volodymyr Boichentsov on 10/22/11.
+//  Copyright (c) 2011 www.injoit.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
